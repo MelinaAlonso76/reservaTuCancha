ebcc37ef3a005f9920c0c7a5c9c9dd31d6d0e6c5
Melina Alonso <72350304+MelinaAlonso76@users.noreply.github.com>
2026-03-04 16:25:10 +0000

    se agrego pestaña reservar e info general

--- app.py
+++ app.py
@@ -3,17 +3,97 @@ import flet as ft
 def main(page: ft.Page):
     page.padding = 0
     page.spacing = 0
-
+    page.bgcolor = "white"
+    
+    #HEADER
     header = ft.Container(
-        width=page.width,        # ocupa todo el ancho
-        height=220,              # altura definida
-        content=ft.Image(
-            src="https://images.unsplash.com/photo-1507525428034-b723cf961d3e",
-            fit=ft.ImageFit.COVER,  # cubre todo el espacio sin deformarse
-            expand=True
+                height=220,
+                width=float("inf"),
+                content=ft.Image(
+                    src="imgcabecera.jpeg",
+                    fit="cover",
+                    width=float("inf")
+                ),
+            )
+
+    #UBICACION
+    location_section=ft.Container(
+        padding=15,
+        content=ft.Column(
+            spacing=5,
+            controls=[
+                ft.Text(
+                    "Complejo Zamudio",
+                    size=36,
+                    weight=ft.FontWeight.BOLD,
+                    color = ft.Colors.BLACK,
+                ),
+                ft.Text(
+                    "📍General Bartolome Mitre 765",
+                    size=20,
+                    color = ft.Colors.BLACK,
+                    weight=ft.FontWeight.NORMAL,
+                ),
+            ],
+        ),
+    )
+
+    #PESTAÑA CON BOTONES #RESERVAR E #INFORMACION
+    #CONTENIDO RESERVAR
+    reservar_view = ft.Column(
+        visible=True,
+        controls=[
+            ft.Text(
+                "Amigo reservaaaa",
+                size=20,
+                weight=ft.FontWeight.BOLD,
+            )
+        ],
+    )
+
+    #CONTENIDO INFO
+    info_view = ft.Column(
+        visible=False,
+        controls=[
+            ft.Text(
+                "Lee antes de reservar papuuu",
+                size=20,
+                weight=ft.FontWeight.BOLD,
+            )
+        ],
+    )
+
+    #FUNCIONES BOTONES
+    def show_reservar(e):
+        reservar_view.visible=True
+        info_view.visible=False
+        page.update()
+
+    def show_info(e):
+        reservar_view.visible=False
+        info_view.visible=True
+        page.update()
+
+    #BOTONES
+    botones = ft.Row(
+        alignment=ft.MainAxisAlignment.CENTER,
+        controls=[
+            ft.ElevatedButton("RESERVAR",on_click=show_reservar),
+            ft.ElevatedButton("INFO GENERAL",on_click=show_info),
+        ],
+    )
+
+    page.add(
+        ft.Column(
+            spacing=0,
+            controls=[
+                header,
+                location_section,
+                botones,
+                reservar_view,
+                info_view,
+            ],
         )
     )
 
-    page.add(header)
-
-ft.app(target=main)
+ft.app(target=main, assets_dir="assets")
